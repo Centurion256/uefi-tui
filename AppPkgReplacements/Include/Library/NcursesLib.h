e5370dc2deef82bf870ba17740f5ae72d3e42abf
Annnnya <anna.polova@ucu.edu.ua>
2024-01-10 18:26:34 +0200

    functionality

--- AppPkgReplacements/Include/Library/NcursesLib.h
+++ AppPkgReplacements/Include/Library/NcursesLib.h
@@ -12,9 +12,60 @@
 #include  <Library/UefiRuntimeServicesTableLib.h>
 #include  <Library/ShellCEntryLib.h>
 #include  <Library/BaseLib.h>
+#include <Protocol/SimpleTextInEx.h>
+#include <Library/PrintLib.h>
+// #include <stdbool.h>
 
-UINTN PrintCharAt (
-        CHAR16    Character
-);
+
+// Define a WINDOW structure for representing a window
+typedef struct WINDOW {
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *SimpleTextOut;
+    EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *SimpleTextInEx;
+    UINTN height;
+    UINTN width;
+    UINTN cursor_y;
+    UINTN cursor_x;
+    CHAR16 *content;
+    UINTN *attributes; 
+} WINDOW;
+
+// Initializers
+WINDOW* initscr(void);  // Start curses mode
+UINTN raw(void);          // Enter raw mode
+UINTN cbreak(void);       // Enter cbreak mode
+UINTN echo(void);         // Enable echoing of characters typed by the user
+UINTN noecho(void);       // Disable echoing
+UINTN keypad(WINDOW *win, BOOLEAN bf);  // Enable reading of function keys
+UINTN endwin(void);       // End ncurses mode
+
+// Output functions
+UINTN addch(CHAR16 ch);                // Put a single character into the current cursor location
+UINTN mvaddch(UINTN y, UINTN x, CHAR16 ch); // Move cursor to a given poUINTN and then print
+UINTN mvprintw(UINTN y, UINTN x, const CHAR16 *format, ...); // Move cursor to a position and then print
+UINTN printw(const CHAR16 *format, ...); // Simple print
+UINTN addstr(const CHAR16 *str);        // Put a character string into a window
+UINTN addnstr(const CHAR16 *str, UINTN n); // Put at most n characters into the screen
+void clear(void);                   // Clear all text
+void refresh(void);                 // Actually prUINTN the window
+
+// Input functions
+UINTN getch(void);                    // Read a single character from the terminal
+UINTN fgetc(IN EFI_FILE_PROTOCOL *File);            // Get character from file.
+UINTN scanw(const CHAR16 *format, ...); // Similar to scanf() with the added capability of getting input from any location on the screen
+UINTN mvscanw(UINTN y, UINTN x, const CHAR16 *format, ...); // Similar to scanw() with cursor movement
+UINTN getstr(CHAR16 *str);               // Get strings from the terminal
+
+// Attributes
+void attron(UINTN attributes);        // Turn on printing with some effect
+void attroff(UINTN attributes);       // Turn off printing with some effect
+void attrset(UINTN attributes);       // Set the attributes of the window
+void attr_get(UINTN *attributes, short *color_pair); // Get current attributes and color pair
+UINTN chgat(UINTN num_chars, UINTN attributes, short color_pair, const void *opts); // Change attributes for characters on the screen
+
+// Colors
+BOOLEAN has_colors(void);               // Check if the terminal supports color
+void start_color(void);              // Initialize color support
+UINTN init_color(short color, UINTN r, UINTN g, UINTN b); // Define a new color
+UINTN init_pair(short pair, short fcolor, short bcolor); // Define foreground and background for a color pair
 
 #endif //UEFI_TUI_NCURSES_H
--- AppPkgReplacements/Library/NcursesLib/NcursesLib.c
+++ AppPkgReplacements/Library/NcursesLib/NcursesLib.c
@@ -4,21 +4,253 @@
 
 #include "Library/NcursesLib.h"
 
-/**
-  Prints a character to the default console, at
-  the supplied cursor position, using L"%c" format.
+// Global variable to hold the WINDOW structure
+WINDOW *gWindow = NULL;
 
-  @param  Column     The cursor position to print the string at.
-  @param  Row        The cursor position to print the string at.
-  @param  Character  Character to print.
+EFI_SIMPLE_TEXT_INPUT_PROTOCOL *gSimpleTextIn;
+//EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *gSimpleTextInEx;
+EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *gSimpleTextOut;
 
-  @return Length of string printed to the console.
 
-**/
-UINTN
-PrintCharAt (
-        CHAR16    Character
-)
+WINDOW* initscr(void)
 {
-    return Print(L"%c", Character);
+    // Locate the Simple Text Input and Output protocols
+    EFI_STATUS Status = gBS->LocateProtocol(
+            &gEfiSimpleTextInProtocolGuid,
+            NULL,
+            (VOID **)&gSimpleTextIn
+    );
+    if (EFI_ERROR(Status)) {
+        return NULL;
+    }
+
+    Status = gBS->LocateProtocol(
+            &gEfiSimpleTextOutProtocolGuid,
+            NULL,
+            (VOID **)&gSimpleTextOut
+    );
+    if (EFI_ERROR(Status)) {
+        return NULL;
+    }
+
+//    Status = gBS->LocateProtocol(
+//            &gEfiSimpleTextInputExProtocolGuid,
+//            NULL,
+//            (VOID **)&gSimpleTextInEx
+//    );
+//    if (EFI_ERROR(Status)) {
+//        return NULL;
+//    }
+
+    // Allocate memory for the WINDOW structure
+    gWindow = AllocatePool(sizeof(WINDOW));
+    if (gWindow == NULL) {
+        return NULL;
+    }
+
+    // Initialize the WINDOW structure
+//    win->SimpleTextOut = gSimpleTextOut;
+    UINTN ScreenWidth;
+    UINTN ScreenRows;
+
+    gST->ConOut->SetCursorPosition (gST->ConOut,0,0);
+    gST->ConOut->QueryMode (gST->ConOut, gST->ConOut->Mode->Mode, &ScreenWidth, &ScreenRows );
+    gWindow->height = ScreenRows;
+    gWindow->width = ScreenWidth;
+    gWindow->cursor_y = 0;
+    gWindow->cursor_x = 0;
+    gWindow->content = AllocatePool(sizeof(CHAR16) * (ScreenRows - 1) * ScreenWidth);
+    gWindow->attributes = AllocatePool(sizeof(UINTN*) * (ScreenRows - 1) * ScreenWidth);
+    UINTN index;
+    for (index = 0; index < (ScreenRows - 1) * ScreenWidth; index++) {
+        gWindow->content[index] = L' ';
+        gWindow->attributes[index] = 0;
+    }
+
+    gWindow->SimpleTextOut = gSimpleTextOut;
+//    gWindow->SimpleTextInEx = gSimpleTextInEx;
+
+    return gWindow;
+}
+
+
+// End ncurses mode
+UINTN endwin(void) {
+    if (gWindow == NULL) {
+        return -1; // curses mode not initialized
+    }
+
+
+    FreePool(gWindow->content);
+    FreePool(gWindow->attributes);
+
+    FreePool(gWindow);
+
+    return 0;
+}
+
+
+
+// Enter raw mode
+UINTN raw(void) {
+    if (gWindow == NULL || gWindow->SimpleTextInEx == NULL) {
+        return -1; // curses mode not initialized or SimpleTextInEx not available
+    }
+
+    EFI_STATUS Status = gWindow->SimpleTextInEx->SetState(gWindow->SimpleTextInEx, NULL);
+
+    return EFI_ERROR(Status) ? -1 : 0;
+}
+
+// Enter cbreak mode
+UINTN cbreak(void) {
+    if (gWindow == NULL || gWindow->SimpleTextInEx == NULL) {
+        return -1; // curses mode not initialized or SimpleTextInEx not available
+    }
+
+    // Set the console input mode to non-raw
+    EFI_STATUS Status = gWindow->SimpleTextInEx->Reset(gWindow->SimpleTextInEx, FALSE);
+    
+    return EFI_ERROR(Status) ? -1 : 0;
+}
+
+// Enable echoing of characters typed by the user
+UINTN echo(void) {
+    if (gWindow == NULL || gWindow->SimpleTextInEx == NULL) {
+        return -1; // curses mode not initialized or SimpleTextInEx not available
+    }
+
+    // Enable echoing
+    gWindow->SimpleTextInEx->SetState(gWindow->SimpleTextInEx, NULL);
+
+    return 0;
+}
+
+// Disable echoing
+UINTN noecho(void) {
+    if (gWindow == NULL || gWindow->SimpleTextInEx == NULL) {
+        return -1; // curses mode not initialized or SimpleTextInEx not available
+    }
+
+    // Disable echoing
+    gWindow->SimpleTextInEx->SetState(gWindow->SimpleTextInEx, NULL);
+
+    return 0;
+}
+
+UINTN keypad(WINDOW *win, BOOLEAN enable) {
+    if (win == NULL || win->SimpleTextInEx == NULL) {
+        return -1; // curses mode not initialized or SimpleTextInEx not available
+    }
+
+    if (enable) {
+        win->SimpleTextInEx->Reset(win->SimpleTextInEx, FALSE); // Enable the input
+    } else {
+        win->SimpleTextInEx->Reset(win->SimpleTextInEx, TRUE); // Disable the input
+    }
+
+    return 0;
+}
+
+
+EFI_STATUS addch(CHAR16 ch)
+{
+    // Put a single character into the current cursor location
+    UINTN index = gWindow->cursor_y * gWindow->width + gWindow->cursor_x;
+    gWindow->content[index] = ch;
+    gWindow->attributes[index] = 0;
+    gWindow->cursor_x++;
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS mvaddch(UINTN y, UINTN x, CHAR16 ch)
+{
+    // Move cursor to a given poUINTN and then print
+    gWindow->cursor_y = y;
+    gWindow->cursor_x = x;
+    return addch(ch);
+}
+
+void refresh(void)
+{
+    // Actually prUINTN the window
+//    EFI_STATUS Status;
+//    UINTN index;
+    gWindow->SimpleTextOut->ClearScreen(gWindow->SimpleTextOut);
+    gWindow->SimpleTextOut->SetCursorPosition(gWindow->SimpleTextOut, 0, 0);
+//    for (index = 0; index < gWindow->width * (gWindow->height - 1); index++) {
+////        gWindow->SimpleTextOut->SetAttribute(gWindow->SimpleTextOut, gWindow->attributes[index]);
+////        gWindow->SimpleTextOut->OutputString(gWindow->SimpleTextOut, L"%c", gWindow->content[index]);
+//    }
+    gWindow->SimpleTextOut->OutputString(gWindow->SimpleTextOut, gWindow->content);
+
+}
+
+// UINTN mvprintw(UINTN y, UINTN x, const CHAR16 *format, ...)
+// {
+//     // Move cursor to a position and then print
+//     gWindow->cursor_y = y;
+//     gWindow->cursor_x = x;
+//     VA_LIST args;
+
+//     return printw(format, args);
+// }
+
+UINTN printw(const CHAR16 *format, ...)
+{
+    VA_LIST args;
+    UINTN returnVal;
+    CHAR16 Buffer[1024]; // Buffer to hold the output
+
+    // Initialize the argument list
+    VA_START(args, format);
+
+    // Print the formatted output to the buffer
+    returnVal = UnicodeVSPrint(Buffer, sizeof(Buffer), format, args);
+
+    // Clean up the argument list
+    VA_END(args);
+
+    for (UINTN i = 0; i < returnVal; i++) {
+        addch(Buffer[i]);
+    }
+
+    return returnVal;
+}
+
+UINTN addstr(const CHAR16 *str)
+{
+    // Put a character string into a window
+    UINTN len = StrLen(str);
+    for (UINTN i = 0; i < len; i++) {
+        addch(str[i]);
+    }
+
+    return len;
+}
+
+UINTN addnstr(const CHAR16 *str, UINTN n)
+{
+    // Put at most n characters into the screen
+    UINTN len = StrnLenS(str, n);
+    for (UINTN i = 0; i < len; i++) {
+        addch(str[i]);
+    }
+
+    return len;
+}
+
+void clear(void)
+{
+    // Clear all text
+    for (UINTN y = 0; y < gWindow->height; y++) {
+        for (UINTN x = 0; x < gWindow->width; x++) {
+            UINTN index = y * gWindow->width + x;
+            gWindow->content[index] = L' ';
+            gWindow->attributes[index] = 0;
+        }
+    }
+
+    gWindow->cursor_y = 0;
+    gWindow->cursor_x = 0;
 }
